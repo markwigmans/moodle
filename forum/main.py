9441006658b7d814b535c268456a54c7f3e64e5b
markwigmans <mark.wigmans@gmail.com>
2022-12-31 21:41:31 +0100

    Add link, replace standard input excel file and simulate 'autofit'

--- forum/main.py
+++ forum/main.py
@@ -10,8 +10,11 @@ SURNAME = "Surname"
 MARKER = "Marker"
 TOTAL = "Forums"
 FORUM = "Forum"
-MESSAGE = 'message'
-
+SUBJECT = 'Subject'
+MESSAGE = 'Message'
+WORD_COUNT = 'Words'
+LINK = 'Link'
+PREFIX_LINK = "https://courses.unic.ac.cy/mod/forum/discuss.php?d="
 
 def normalize_key(key: str, remove_chars: str = string.whitespace) -> str:
     """Remove all specified characters from the input string and return the result in lowercase."""
@@ -35,7 +38,9 @@ def get_students(filename:str) -> pd.core.groupby.generic.DataFrameGroupBy:
     df = pd.read_csv(filename)
     # groupBy() can't handle empty DataFrames it seems, so we have to create a trick
     if len(df) > 0:
+        df = df.rename(columns={'wordcount': WORD_COUNT, 'message' : MESSAGE, 'subject' : SUBJECT})
         df[KEY] = df.apply(lambda row: normalize_key(row["userfullname"]), axis=1)
+        df[LINK] = df.apply(lambda row: f"{PREFIX_LINK}{row['discussion']}", axis=1)
         df[MESSAGE] = df[MESSAGE].apply(lambda x: x.strip())
     else:
         df = pd.DataFrame({KEY: []})
@@ -72,12 +77,16 @@ def calc_participation(overview, sheets):
     return df
 
 
-def filter_range(prefix:str, worksheet) -> str:
-    """Create the range the given worksheet will have a autofilter range"""
+def set_filter_range(begin_col:int, last_col:int, worksheet) -> None:
+    """Set worksheet autofilter range"""
     if worksheet.dim_rowmax > 0:
-        return prefix + str(worksheet.dim_rowmax)
-    else:
-        return prefix + '1'    
+        worksheet.autofilter(0, begin_col, worksheet.dim_rowmax, last_col)
+  
+
+def get_size_by_values(col:str, df) -> int :
+    values = df[col].values
+    longest_string = max(values, key=len)
+    return len(longest_string)
 
 
 def gen_sheet(filename, participation, overview, sheets):
@@ -95,9 +104,13 @@ def gen_sheet(filename, participation, overview, sheets):
     for index, (sheet, description, _) in enumerate(sheets):
         worksheet.write_string(index + 4, 0, f"{sheet} : {description}")
 
-    fmt_message = writer.book.add_format({'valign': 'top','text_wrap': True})
-    fmt_text = writer.book.add_format({'valign': 'top'})
-    fmt_number = writer.book.add_format({'align': 'center','valign': 'top'})
+    # set workbook formats
+    fmt_message = writer.book.add_format({'valign' : 'top','text_wrap' : True})
+    fmt_text = writer.book.add_format({'valign' : 'top'})
+    fmt_number = writer.book.add_format({'align' : 'center','valign' : 'top'})
+    fmt_link = writer.book.get_default_url_format()
+    fmt_link.set_align('top')
+    writer.book.default_url_format = fmt_link
 
     #
     # Overview part
@@ -109,9 +122,11 @@ def gen_sheet(filename, participation, overview, sheets):
         index=False)
     worksheet = writer.sheets['Overview']
     worksheet.freeze_panes(1, 0)
+    worksheet.set_column('A:A', get_size_by_values(FIRST_NAME, participation))
+    worksheet.set_column('B:B', get_size_by_values(SURNAME, participation))
     column_letter = xlsxwriter.utility.xl_col_to_name(len(sheets) + 2)
-    worksheet.set_column(f"D:{column_letter}", None, fmt_number) 
-    worksheet.autofilter(filter_range('C1:C', worksheet))
+    worksheet.set_column(f"D:{column_letter}", None, fmt_number)
+    set_filter_range(2, 2, worksheet)
 
     #
     # generate sheets with all posts
@@ -133,29 +148,35 @@ def gen_sheet(filename, participation, overview, sheets):
     posts.to_excel(
         writer,
         sheet_name="Posts",
-        columns=[FIRST_NAME, SURNAME, MARKER, FORUM, 'subject', 'message', 'wordcount'],
+        columns=[FIRST_NAME, SURNAME, MARKER, FORUM, SUBJECT, MESSAGE, WORD_COUNT, LINK],
         index=False)        
 
     worksheet = writer.sheets['Posts']
     worksheet.freeze_panes(1, 0)
-    worksheet.set_column('A:G', None, fmt_text) 
-    worksheet.set_column('E:E', 40) 
+    worksheet.set_column('A:G', None, fmt_text)
+    worksheet.set_column('A:A', get_size_by_values(FIRST_NAME, participation))
+    worksheet.set_column('B:B', get_size_by_values(SURNAME, participation))
+    worksheet.set_column('E:E', 40, fmt_message) 
     worksheet.set_column('F:F', 100, fmt_message)
-    worksheet.autofilter(filter_range('C1:D', worksheet))
+    worksheet.set_column('H:H', 60)
+    set_filter_range(2, 3, worksheet)
 
     #
     # generate sheets with the data
     #
     for sheet,_,students in sheets:
         posts.loc[posts[FORUM] == sheet].to_excel(writer, sheet_name=sheet,
-            columns=[FIRST_NAME, SURNAME, MARKER, 'subject', 'message', 'wordcount'], 
+            columns=[FIRST_NAME, SURNAME, MARKER, SUBJECT, MESSAGE, WORD_COUNT, LINK], 
             index=False)
         worksheet = writer.sheets[sheet]
         worksheet.freeze_panes(1, 0)
-        worksheet.set_column('A:F', None, fmt_text) 
-        worksheet.set_column('D:D', 40) 
+        worksheet.set_column('A:G', None, fmt_text) 
+        worksheet.set_column('A:A', get_size_by_values(FIRST_NAME, participation))
+        worksheet.set_column('B:B', get_size_by_values(SURNAME, participation))
+        worksheet.set_column('D:D', 40, fmt_message) 
         worksheet.set_column('E:E', 100, fmt_message)
-        worksheet.autofilter(filter_range('C1:C', worksheet))
+        worksheet.set_column('G:G', 60)
+        set_filter_range(2, 2, worksheet)
          
     worksheet = writer.sheets['Overview']
     worksheet.activate()
@@ -164,7 +185,7 @@ def gen_sheet(filename, participation, overview, sheets):
 
 def main(args) -> None:
     """Main application"""
-    overview = read_grade_sheet("bloc-516 - Fall 2022 - Exam Marks.xlsx")
+    overview = read_grade_sheet("students.xlsx")
     sheets_list = [
         ("P-1", "Session 4  - Creating Money", get_students("discussion-1.csv")),
         ("P-2", "Session 6  - Securitization", get_students("discussion-2.csv")),
