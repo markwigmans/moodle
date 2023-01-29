26aa0ba6e9d558714da112924ead16b441cd8f1d
markwigmans <mark.wigmans@gmail.com>
2023-01-29 19:48:17 +0100

    Remove hardcoded constants

---
+++ exam/main.py
@@ -0,0 +1,20 @@
+"""main file of exam application"""
+
+from GradeSheet import *
+from Exams import *
+import configparser
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    grades_cfg = config['grades']
+    grades = GradeSheet(grades_cfg['file'], grades_cfg['worksheet'])
+    students, markers = grades.read()
+
+    files_cfg = config['files']
+    exams = Exams(files_cfg['data'], files_cfg['output'], students, markers)
+    exams.process(files_cfg['not_processed'])
+    exams.gen_overview(files_cfg['overview'])
+
+if __name__ == '__main__':
+    main()
