a5af1b9c1e78a08b7f135181519eb759b40a4d67
markwigmans <mark.wigmans@gmail.com>
2024-01-25 14:59:57 +0100

    Better formula

--- exam/Exams.py
+++ exam/Exams.py
@@ -79,7 +79,8 @@ class Exams:
                 start_cell = Utils.to_cell(y_offset, i)
                 end_cell =  Utils.to_cell(worksheet.dim_rowmax, i)
                 # calculate work to do
-                worksheet.write(0,i,f"=SUBTOTAL(103,{start_cell}:{end_cell})-SUBTOTAL(102,{start_cell}:{end_cell})")
+                r = f"{start_cell}:{end_cell}"
+                worksheet.write(0,i,f'=SUMPRODUCT(SUBTOTAL(3,OFFSET({r},ROW({r})-ROW({start_cell}),0,1)),--({r}="X"))')
             worksheet.write(0, len(exam_columns)+x_offset, f"=SUM({Utils.to_cell(0,x_offset)}:{Utils.to_cell(0,len(exam_columns)+x_offset-1)})")
 
             # Write formulas
--- exam/GradeSheet.py
+++ exam/GradeSheet.py
@@ -1,13 +1,21 @@
 import pandas as pd
 
 class GradeSheet:
-    """Process Teams Gradesheet"""
+    """
+    A class to process a gradesheet for teams.
+
+    Attributes:
+        filename (str): The name of the file containing the gradesheet.
+        sheet_name (str): The name of the sheet within the Excel file.
+        header (int): The row number of the header.
+    """
 
     # used column names as constants
     FIRST_NAME = "First name"
     SURNAME = "Surname"
     ID_NUMBER = "ID number"
     MARKER = "Marker"
+    DEFAULT_MARKER = "-"
 
     def __init__(self, filename:str, sheet_name:str, header:int):
         self.filename = filename
@@ -15,15 +23,22 @@ class GradeSheet:
         self.header = header
 
     def read(self):
-        worksheet = (pd
-                     .read_excel(self.filename, sheet_name=self.sheet_name, header=self.header)
-                     .dropna(subset=[self.FIRST_NAME, self.SURNAME]))
-        # clean data
+        """
+        Reads and processes the gradesheet.
+
+        Returns:
+            tuple: A tuple containing dictionaries of students and markers.
+        """
+        worksheet = pd.read_excel(self.filename, sheet_name=self.sheet_name, header=self.header)
+        worksheet = worksheet.dropna(subset=[self.FIRST_NAME, self.SURNAME])
+
+        # Clean and standardize data
         worksheet[self.FIRST_NAME] = worksheet[self.FIRST_NAME].str.title()
         worksheet[self.SURNAME] = worksheet[self.SURNAME].str.title()
         worksheet[self.ID_NUMBER] = worksheet[self.ID_NUMBER].fillna('')
-        worksheet[self.MARKER] = worksheet[self.MARKER].fillna('-')
+        worksheet[self.MARKER] = worksheet[self.MARKER].fillna(self.DEFAULT_MARKER)
 
+        # Create dictionaries for students and markers
         students = {row[self.ID_NUMBER] : row.to_dict() for _, row in worksheet.iterrows()}
         markers  = {row[self.MARKER] : row.to_dict() for _, row in worksheet.iterrows()}
         return students, markers
