1cde51c8c17a2c37f7227a62399a29d6f1b75298
markwigmans <mark.wigmans@gmail.com>
2025-01-26 17:05:56 +0100

    Improve exam code

--- exam/Exams.py
+++ exam/Exams.py
@@ -3,6 +3,7 @@ import os
 import re
 from pathlib import Path
 from shutil import copyfile
+from typing import Final
 
 import pandas as pd
 
@@ -12,9 +13,8 @@ from utils.Utils import Utils
 
 class Exams:
     """Process student exams"""
-
-    TOTAL = "Total"
-    EXAMS = "#Exams"
+    TOTAL: Final[str] = "Total"
+    EXAMS: Final[str] = "#Exams"
 
     def __init__(self, source, target, students, markers):
         self.source = source
@@ -23,7 +23,7 @@ class Exams:
         self.markers = markers
         self.processed = set()
         self.exams = set()
-        self.student_exams = dict()
+        self.student_exams = {}
 
     def process(self, filename: str, min_length: int) -> None:
         """Process all files in source directory"""
@@ -42,18 +42,78 @@ class Exams:
         for exam in exam_columns:
             overview.insert(len(overview.columns), exam, '', True)
 
+        overview.loc[self.students.keys(), self.TOTAL] = 0
+        overview.loc[self.students.keys(), self.EXAMS] = ''
+        for student, exams in self.student_exams.items():
+            overview.loc[student, list(exams)] = 'X'
+
+        self._write_excel(filename, overview, exam_columns)
+
+    def _process_file(self, path: Path) -> None:
+        """Process file if it is a question text"""
+        if re.match(r"Question text", path.stem):
+            for marker in self.markers:
+                self._copy_file(path, marker)
+
+    def _process_dir(self, path: Path, min_length: int) -> None:
+        """Process dir if it is a student directory"""
+        result = re.match(r"(\w{9}) - (.*)", path.stem)
+        if result:
+            student_id = result.group(1)
+            if student := self.students.get(student_id):
+                if self._dir_meets_size(path, min_length):
+                    marker = student[GradeSheet.MARKER]
+                    self._copy_dir(path, marker)
+                    self._add_exam(student_id, path)
+                    self.processed.add(student_id)
+                else:
+                    logging.info(f"Under minimal length({min_length}): '{path}'")
+            else:
+                logging.warning(f"Student '{student_id}' not found!")
+
+    def _copy_file(self, file, marker) -> None:
+        """copy file to target directory"""
+        directory = os.path.dirname(file).split(os.sep)[-1]
+        new = Path(self.target, marker, directory)
+        new.mkdir(parents=True, exist_ok=True)
+        copyfile(file, Path(new, file.name))
+
+    def _copy_dir(self, path, marker) -> None:
+        """Copy dir to target directory"""
+        question_dir = os.path.dirname(path).split(os.sep)[-1]
+        new = Path(self.target, marker, question_dir, path.name)
+        new.mkdir(parents=True, exist_ok=True)
+        for file in path.iterdir():
+            copyfile(file, Path(new, file.name))
+
+    def _dir_meets_size(self, path: Path, min_length: int) -> bool:
+        """Check if any file in the directory meets the size requirement"""
+        for file_path in path.iterdir():
+            if file_path.is_file():
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    content = file.read().strip()
+                    logging.debug(f"len is: {len(content)} : {content[:20]}")
+                    if len(content) >= min_length:
+                        return True
+            else:
+                logging.error(f"File: '{file_path}' is NOT a file")
+        return False
+
+    def _save_unprocessed(self, filename: str) -> None:
+        """save all students that were not processed"""
+        unprocessed = pd.DataFrame.from_dict(self.students, orient='index')
+        unprocessed.loc[~unprocessed.index.isin(self.processed)].to_excel(filename, index=False)
+
+    def _add_exam(self, student, path) -> None:
+        """Add exam to student and exam list"""
+        exam = os.path.dirname(path).split(os.sep)[-1].split(' ')[-1]
+        self.exams.add(exam)
+        self.student_exams.setdefault(student, set()).add(exam)
+
+    def _write_excel(self, filename: str, overview, exam_columns: list[str]):
+        """create Excel file"""
         y_offset = 3
         x_offset = 5
-
-        for student in self.students:
-            # I can't add a formula here, pandas changes the content, don't know why
-            overview.loc[student, self.TOTAL] = 0
-            overview.loc[student, self.EXAMS] = ''
-            for exam in exam_columns:
-                if exam in self.student_exams.get(student, []):
-                    overview.loc[student, exam] = 'X'
-
-        # create file
         with pd.ExcelWriter(filename, engine="xlsxwriter") as writer:
             overview.to_excel(
                 writer,
@@ -98,71 +158,3 @@ class Exams:
                                              {'type': 'cell', 'criteria': '!=', 'value': 3, 'format': fmt_signal_text})
 
             Utils.set_filter_range(x_offset - 2, len(exam_columns) - 1 + x_offset, worksheet, y_offset - 1)
-
-    def _process_file(self, path: Path) -> None:
-        """Process file if it is a question text"""
-        if re.match(r"Question text", path.stem):
-            for marker in self.markers:
-                self._copy_file(path, marker)
-
-    def _process_dir(self, path: Path, min_length: int) -> None:
-        """Process dir if it is a student directory"""
-        result = re.match(r"(\w{9}) - (.*)", path.stem)
-        if result:
-            student_id = result.group(1)
-            if self.students.get(student_id):
-                if self._dir_meets_size(path, min_length):
-                    marker = self.students[student_id][GradeSheet.MARKER]
-                    self._dir_meets_size(path, 50)
-                    self._copy_dir(path, marker)
-                    self._add_exam(student_id, path)
-                    self.processed.add(student_id)
-                else:
-                    logging.info(f"Under minimal length({min_length}): '{path}'")
-            else:
-                logging.warning(f"Student '{student_id}' not found!")
-
-    def _copy_file(self, file, marker) -> None:
-        """copy file to target directory"""
-        directory = os.path.dirname(file).split(os.sep)[-1]
-        new = Path(self.target, marker, directory)
-        new.mkdir(parents=True, exist_ok=True)
-        copyfile(file, Path(new, file.name))
-
-    def _copy_dir(self, path, marker) -> None:
-        """Copy dir to target directory"""
-        question_dir = os.path.dirname(path).split(os.sep)[-1]
-        new = Path(self.target, marker, question_dir, path.name)
-        new.mkdir(parents=True, exist_ok=True)
-        for file in path.iterdir():
-            p = Path(new, file.name)
-            copyfile(file, p)
-
-    def _dir_meets_size(self, path: Path, min_length: int) -> bool:
-        """Check if any file in the directory meets the size requirement"""
-        for file_path in path.iterdir():
-            if file_path.is_file():
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    content = file.read().strip()
-                    logging.debug(f"len is: {len(content)} : {content[:20]}")
-                    if len(content) >= min_length:
-                        return True
-            else:
-                logging.error(f"File: '{file_path}' is NOT a file")
-        return False
-
-    def _save_unprocessed(self, filename: str) -> None:
-        """save all students that were not processed"""
-        unprocessed = pd.DataFrame.from_dict(self.students, orient='index')
-        result = unprocessed.loc[~unprocessed.index.isin(self.processed)]
-        result.to_excel(filename, index=False)
-
-    def _add_exam(self, student, path) -> None:
-        """Add exam to student and exam list"""
-        exam_dir = os.path.dirname(path).split(os.sep)[-1]
-        exam = exam_dir.split(' ')[-1]
-        self.exams.add(exam)
-        if student in self.student_exams:
-            self.student_exams[student].add(exam)
-        else:
-            self.student_exams[student] = {exam}
